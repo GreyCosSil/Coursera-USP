472fb96000fe5dd3c98057a7bd2e07787ced105c
GreyCosSil <52082468+GreyCosSil@users.noreply.github.com>
2020-03-08 17:44:26 -0300

    Tarefa de programação: Lista de exercícios - 2

---
+++ Exercício5-crescente.py
@@ -0,0 +1,9 @@
+firstNumber = int(input('Digite o primeiro número inteiro: '))
+SecondNumber = int(input('Digite o Segundo número inteiro: '))
+ThirdNumber = int(input('Digite o Terceiro número inteiro: '))
+
+if(firstNumber<SecondNumber<ThirdNumber):
+	print('crescente')
+else:	
+	print('não está em ordem crescente')
+
